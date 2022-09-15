ff7683fbc211dae635ce76aa651a5bfc0e5dc5d2
Pedro Borges <pedrogilaab@gmail.com>
2022-09-15 15:19:04 -0300

    pegando os dados da api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
+import {useEffect, useState} from "react";
 
 function App() {
+
+  const [joke, setJoke] = useState("Joke's here!") //constantes
+
+  useEffect(() => {
+    getJoke();
+  }, []);
+
+  const getJoke = async () =>{
+    const response = await fetch("https://icanhazdadjoke.com", {
+      headers: {
+        Accept: "application/json",
+      },
+    });
+    const data = await response.json();
+    console.log(data);
+    setJoke(data.joke);
+  }; //piada aparecer
+
+
+
   return (
-    <div>
-      <h1 className="container">Hello World!</h1>
+    <div className="container">
+      <h1>Don't laugh challenge</h1>
+      <p className="joke">{joke}</p>
+      <button className="btn">Get Another Joke</button>
     </div>
   );
 }
